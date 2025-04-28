078c0c3bcd96ee7e79859678c46dbd60e92184db
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-04-29 01:30:03 +0800

    feat: implement nativewind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import '../global.css'
 
 const styles = StyleSheet.create({
 	container: {
@@ -13,7 +14,9 @@ const styles = StyleSheet.create({
 function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
+			<Text className="text-red-500">
+				Open up App.tsx to start working on your app!!!
+			</Text>
 			<StatusBar style="auto" />
 		</View>
 	)
